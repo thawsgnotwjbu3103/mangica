8e84e2e16a48a0063658fc38a4a4e1b76c750ee3
Thawsg <thang.nd3103@gmail.com>
2023-03-03 16:51:06 +0700

    rebase client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { Route, Switch } from "wouter"
+import Home from "@/pages/user/Home"
+import Title from "@/pages/user/Title"
+import Read from "@/pages/user/Read"
+import Login from "@/pages/user/Login"
+import Register from "@/pages/user/Register"
+import News from "@/pages/user/News"
+import Search from "@/pages/user/Search"
+import NotFound from "@/pages/user/NotFound"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      
-    </div>
-  )
+    return (
+        <>
+            <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/title/:titleId" component={Title} />
+                <Route path="/read/:titleId/:chapterId" component={Read} />
+                <Route path="/search" component={Search} />
+                <Route path="/news" component={News} />
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+                <Route component={NotFound} />
+            </Switch>
+        </>
+    )
 }
 
 export default App
---
+++ client/src/pages/user/Read.jsx
@@ -0,0 +1,7 @@
+function Read() {
+  return (
+    <div>Read</div>
+  )
+}
+
+export default Read
